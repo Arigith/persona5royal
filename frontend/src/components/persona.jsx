59e32effc0e0a8e86c7ea5aee9777ddfb098c6d9
Stacey Turner <112356048+Arigith@users.noreply.github.com>
2022-12-16 09:26:20 +1300

    added select filter

--- frontend/src/components/persona.jsx
+++ frontend/src/components/persona.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 export default function TablePersona() {
     const [personaList, setPersonaList] = useState([""]);
+    const [selectedArcana, setSelectedArcana] = useState("");
 
     async function AllPersona() {
         const response = await fetch("http://127.0.0.1:8000/list_all");
@@ -14,21 +15,57 @@ export default function TablePersona() {
         AllPersona();
     }, []);
 
+    function handleArcanaChange(event) {
+        setSelectedArcana(event.target.value);
+    }
+
     return (
         <>
         <h1>Full Persona List</h1>
+        <label>
+            Arcana:
+            <select value={selectedArcana} onChange={handleArcanaChange}>
+                <option value="">All</option>
+                <option value="Fool">Fool</option>
+                <option value="Magician">Magician</option>
+                <option value="Priestess">Priestess</option>
+                <option value="Empress">Empress</option>
+                <option value="Emperor">Emperor</option>
+                <option value="Hierophant">Hierophant</option>
+                <option value="Lovers">Lovers</option>
+                <option value="Chariot">Chariot</option>
+                <option value="Justice">Justice</option>
+                <option value="Hermit">Hermit</option>
+                <option value="Fortune">Fortune</option>
+                <option value="Strength">Strength</option>
+                <option value="Hanged">Hanged</option>
+                <option value="Death">Death</option>
+                <option value="Temperance">Temperance</option>
+                <option value="Devil">Devil</option>
+                <option value="Tower">Tower</option>
+                <option value="Star">Star</option>
+                <option value="Moon">Moon</option>
+                <option value="Sun">Sun</option>
+                <option value="Judgement">Judgement</option>
+                <option value="Faith">Faith</option>
+                <option value="World">Councillor</option>
+                <option value="World">All</option>
+            </select>
+        </label>
         <table>
             <tr>
                 <th>Arcana</th>
                 <th>Persona Name</th>
                 <th>Starting Level</th>
             </tr>
-            {personaList.map((persona, index) => (
-            <tr key={index}>
-                <td>{persona.arcana}</td>
-                <td>{persona.persona_name}</td>
-                <td>{persona.starting_level}</td>
-            </tr>
+            {personaList
+                .filter(persona => selectedArcana === "" || persona.arcana === selectedArcana)
+                .map((persona, index) => (
+                <tr key={index}>
+                    <td>{persona.arcana}</td>
+                    <td>{persona.persona_name}</td>
+                    <td>{persona.starting_level}</td>
+                </tr>
             ))}
         </table>
         </>
